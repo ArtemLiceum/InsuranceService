a8d7eb923b6bb7a0f696e4390f1feb67e48c507a
ArtemLiceum <bedintema@gmail.com>
2024-11-19 21:50:14 +0300

    last commit 2.0

--- app/main.py
+++ app/main.py
@@ -3,20 +3,17 @@ from app.database import engine, Base, AsyncSessionLocal
 from app.routes import router
 from app.crud import add_rates
 
-app = FastAPI()
+app = FastAPI()c
 
 @app.on_event("startup")
 async def startup_event():
     async with engine.begin() as conn:
-        # Create tables
         await conn.run_sync(Base.metadata.create_all)
 
-    # Add rates to the database
-    db = AsyncSessionLocal()  # Create a new session
+    db = AsyncSessionLocal()
     try:
-        await add_rates(db)  # Add rates asynchronously
+        await add_rates(db)
     finally:
-        await db.close()  # Ensure the session is closed
+        await db.close()
 
-# Include router for handling routes
 app.include_router(router)
